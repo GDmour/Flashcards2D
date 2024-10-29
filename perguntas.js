bcfdb260f67a27f93bbf98d6a4c43d8c83e83ed4
Gabriel Moura <140829776+GDmour@users.noreply.github.com>
2024-10-29 08:27:15 -0300

    f

--- perguntas.js
+++ perguntas.js
@@ -8,3 +8,13 @@ criaCartao(
     'o que é congruência de triângulos?',
     'Na Geometria Plana é dito que dois triângulos são congruentes quando os lados e ângulos do primeiro triângulo estão em correspondência com os lados e ângulos do segundo triângulo de tal forma que os lados e ângulos em correspondência tenham a mesma medida.'
 )
+criaCartao(
+    'Portugues',
+    'qual diferença entre mais e mas?',
+    'Entretanto, possuem significados completamente diferentes, não podendo ser utilizadas uma no lugar da outra. Neste caso, “mas” é usado com um sentido de “porém”, “contudo”, “todavia”, indicando oposição. E “mais” traz a ideia de adição, aumento.'
+)
+criaCartao(
+    'Matemática I',
+    'o que é Clt?',
+    'CLT é a sigla para Consolidação das Leis do Trabalho.' 
+)
