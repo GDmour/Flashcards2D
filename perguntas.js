bc46f2e43c6860e350b13db80e52d0a4f8367faf
Gabriel Moura <140829776+GDmour@users.noreply.github.com>
2024-10-29 08:16:43 -0300

    s

---
+++ perguntas.js
@@ -0,0 +1,10 @@
+criaCartao(
+    'Programaçao',
+    'O que e Visual Studio Code',
+    'e um aplicativo programador de sites'
+    )
+criaCartao(
+    'Matematica',
+    'o que é congruência de triângulos?',
+    'Na Geometria Plana é dito que dois triângulos são congruentes quando os lados e ângulos do primeiro triângulo estão em correspondência com os lados e ângulos do segundo triângulo de tal forma que os lados e ângulos em correspondência tenham a mesma medida.'
+)
