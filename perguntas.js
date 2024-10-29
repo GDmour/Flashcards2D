93d46cf27c2c8f930dc24ed959a4160d64b43217
Gabriel Moura <140829776+GDmour@users.noreply.github.com>
2024-10-29 08:38:29 -0300

    ee

--- perguntas.js
+++ perguntas.js
@@ -18,3 +18,43 @@ criaCartao(
     'o que é Clt?',
     'CLT é a sigla para Consolidação das Leis do Trabalho.' 
 )
+criaCartao(
+    'Física I',
+    'o que é potenciômetro?',
+    'aparelho que mede as diferenças de potencial elétrico em condições de quase reversibilidade e as forças eletromotrizes.'
+)
+criaCartao(
+    'BIOLOGIA',
+    ' O que é ancestral comum?',
+    'Ancestral comum é uma espécie que, através do processo de descendência com modificação, dá origem a novas espécies. '
+)
+criaCartao(
+    'História',
+    'Quem era o líder dos nazistas?',
+    'Adolf Hitler'
+)
+criaCartao(
+    'Química',
+    'qual elemento da tabela periódica tem o número atômico 37',
+    'Rb Rubídio'
+)
+criaCartao(
+    ' Sociologia ',
+    'o que é cidadania?',
+    'Juridicamente, cidadão é o indivíduo no gozo dos direitos civis e políticos de um Estado. Em um conceito mais amplo, cidadania quer dizer a qualidade de ser cidadão, e consequentemente sujeito de direitos e deveres.'
+)
+criaCartao(
+    'educação financeira',
+    ' o que taxa selic',
+    ' taxa Selic é a taxa básica de juros da economia, que influencia outras taxas de juros do país, como taxas de empréstimos, financiamentos e aplicações financeiras. '
+)
+criaCartao(
+    ' geografia',
+    ' o que é desigualdade?',
+    'Desigualdade social é um mal que afeta todo o mundo, em especial os países que ainda encontram-se em vias de desenvolvimento. '
+)
+criaCartao(
+    ' Ingles',
+    ' traduza a frase ‘I would like Drink Water’',
+    'Eu gostaria de beber água'
+)
